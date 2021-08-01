0d0c0fa4a90a0ca93dd9ee295cd53c9d1688e3b6
Milutin <grobar.e@gmail.com>
2021-08-01 19:58:09 +0200

    paper rock scissors w/ ui v1.0

--- main.js
+++ main.js
@@ -1,75 +1,86 @@
-function computerOption() {
+const game = () => {
+
+  let playerScore = 0;
+  let computeScore = 0;
+
+  function resetGame() {
+    playerScore = 0;
+    computeScore = 0;
+    const computerS = document.querySelector('.computer-score span')
+    const playerS = document.querySelector('.player-score span');
+    computerS.innerText = 0;
+    playerS.innerText = 0;    
+ };
+
+  function computerChoose() {
     const options = ["rock", "paper", "scissors"];
     let ranInt = Math.floor(Math.random() * 3);
     return options[ranInt];
-};
-// function cleanInsults() {
-//     const insults = document.querySelector('.fancy');
-//     const button = document.querySelector('.restart');
-//     button.addEventListener('click', function() {
-//         if (!insults) return;
-//         insults.remove();
-//     })
-//}
+  }
 
-function playGame(){
-    let a = 'default';
-    const body = document.querySelector('.content');
-    const btns = Array.from(document.querySelectorAll('.btn'));
-    btns.forEach(btn=> btn.addEventListener('click', function(e) {
-        a = e.target.innerText.toLowerCase();
-        let b = computerOption();
-        
-        if (a === b) {
-            if(confirm("It's a draw! Play again?")) {
-                
-            } else {
-                const newEle = document.createElement('h2');
-                newEle.classList.add('fancy');
-                newEle.textContent = "You are a bitch!"
-                body.appendChild(newEle);
-            }
-        } else if (a==='paper' && b==='rock') {
-            if(confirm("YOU WON! Play again?")) {
-                
-            } else {
-                const newEle = document.createElement('h2');
-                newEle.classList.add('fancy');
-                newEle.textContent = "You are a bitch!"
-                body.appendChild(newEle);
-            }
-        } else if (a === 'scissors' && b === 'paper') {
-            if(confirm("YOU WON! Play again?")) {
-
-                
-            } else {
-                const newEle = document.createElement('h2');
-                newEle.classList.add('fancy');
-                newEle.textContent = "You are a bitch!"
-                body.appendChild(newEle);
-            }
-        } else if (a === 'rock' && b === 'scissors') {
-            if(confirm("YOU WON! Play again?")) {
-                
-            } else {
-                const newEle = document.createElement('h2');
-                newEle.classList.add('fancy');
-                newEle.textContent = "You are a bitch!"  
-                body.appendChild(newEle);
-            }
-        } else {
-            if (confirm(`PC WON! ${b} beats ${a}! Play again?`)) {
-                const vanish = document.querySelector('.fancy')
-                if (!vanish) return;
-                vanish.remove();
-                
-            } else {
-                const newEle = document.createElement('h2');
-                newEle.classList.add('fancy');
-                newEle.textContent = "You are a bitch!"
-                body.appendChild(newEle);
-            }
-        }
-    }));
+  function updateScore() {
+      const computerS = document.querySelector('.computer-score span')
+      const playerS = document.querySelector('.player-score span');
+      playerS.innerText = playerScore;
+      computerS.innerText = computeScore;
+     
+  };
+  
+  function checkWinner(a, b) { //a is player, b is computer
     
-}
+    if (a === b) {
+        return "It's a draw!";
+    };
+    //rock check
+    if (a === 'rock') {
+        if (b === 'scissors') {
+            return 'player';
+        } else {
+            return 'computer'; //if a is not winner, its b
+        };
+    };
+    //paper check
+    if (a === 'paper') {
+        if (b === 'rock') {
+            return 'player';
+        } else {
+            return 'computer';
+        };
+    };
+
+    if (a == 'scissors') {
+        if (b === 'scissors') {
+            return 'player';
+        } else {
+            return 'computer';
+        };
+    };
+
+  };
+
+  playGame = () => {
+    
+    const buttons = Array.from(document.querySelectorAll('.buttons button'));
+    buttons.forEach((button) => {
+      button.addEventListener('click', function(e) {
+        const playerOption = e.target.innerText.toLowerCase();
+        const computerOption = computerChoose();
+        const winner = checkWinner(playerOption, computerOption);
+        const resetButton = document.querySelector('.score button');
+        resetButton.addEventListener('click', resetGame);
+        
+        if (winner === 'player') {
+          playerScore++;
+          updateScore();
+        } else if (winner === 'computer') {
+          computeScore++;
+          updateScore();
+        } else {
+          console.log('Its a tie.')
+        }
+      });
+    });
+  };
+  //function calling
+  playGame();
+};
